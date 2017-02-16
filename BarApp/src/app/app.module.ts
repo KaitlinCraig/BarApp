75995fbd4d47bfa18e6792120e444c1a249f61dd
Knagus <aseawell@ggc.edu>
2017-02-16 00:28:04 -0500

    Added Firebase & Google Plus Authentication
    So, Firebase is working and I've got Google Plus Authentication going.
    Need to style the app a bit and work more database interaction out. Very
    few bugs so far. Had an issue with logging out on my S5, need to check
    that out.
    
    -Need to add basic install instructions into the readme

--- BarApp/src/app/app.module.ts
+++ BarApp/src/app/app.module.ts
@@ -6,6 +6,26 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+// Import the AF2 Module
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+    apiKey: "AIzaSyCvIeTLt_navIsnvRQAb-yHFmEfAPnDoZA",
+    authDomain: "fir-stuff-40bcf.firebaseapp.com",
+    databaseURL: "https://fir-stuff-40bcf.firebaseio.com/",
+    storageBucket: "fir-stuff-40bcf.appspot.com",
+    messagingSenderId: "695531012598"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect, 
+  scope: ['email','id','name','picture'],
+  offline: true
+};
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -15,7 +35,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+	AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- BarApp/src/pages/tabs/tabs.ts
+++ BarApp/src/pages/tabs/tabs.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import { NavController, AlertController, ActionSheetController, Platform } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import {GooglePlus} from 'ionic-native';
+import firebase from 'firebase'
+
+
 import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
